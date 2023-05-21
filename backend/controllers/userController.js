921e9eb0499ef9d21ebe54b48f24385706be2755
Sourabh <sourabhhaldarh@gmail.com>
2023-05-22 03:28:59 +0530

    final commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -151,7 +151,9 @@ exports.logoutUser = (req, res) =>{
     res
      .status(200)
      .cookie("token","",{
-        expires: new Date(Date.now())
+        expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV ==="Development" ? false : true,
      })
      .json({
         success:true,
