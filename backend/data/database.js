d338157c2921c3d1a2a1ec9ad4df6b95802dbd5a
Sourabh <sourabhhaldarh@gmail.com>
2023-05-24 13:37:22 +0530

    fixed config.env

--- backend/data/database.js
+++ backend/data/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = () =>{
-    mongoose.connect(`${process.env.MONGO_URI}/atgworld`).then(()=> {
+    mongoose.connect("mongodb+srv://soourabh:Sourabh@cluster0.gywogyh.mongodb.net/atgworld").then(()=> {
         console.log("Connected to MongoDB database")
     }).catch((err) =>{
         console.log(err)
